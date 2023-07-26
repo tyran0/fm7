03a5e0faea366afb092f47a6acf513e22048eb7e
tyran0 <119806764+tyran0@users.noreply.github.com>
2023-07-26 13:25:16 +0400

    Installed React

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <div>
+      Score
+      Rules
+
+      You Picked
+      The House Picked
+
+      You Win
+      You Lose
+
+      Play Again
+    </div>
+  );
+}
